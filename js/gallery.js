03840f7acc92fe3cb8d50b6cffe7575197c6fd9f
Raym0NT <konasr6@gmail.com>
2023-08-26 11:53:45 +0300

    main page mobile -v

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,50 @@
 Fancybox.bind('[data-fancybox="gallery"]', {
-  // Your custom options
+  backFocus: false,
+});
+
+const gallerySlider = new Swiper(".gallery__slider", {
+  grid: {
+    rows: 2,
+    fill: "row",
+  },
+  slidesPerView: 4,
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".gallery__arrow-next",
+    prevEl: ".gallery__arrow-prev",
+  },
+
+  breakpoints: {
+    0: {
+      grid: {
+        rows: 1,
+        fill: "row",
+      },
+      slidesPerView: 1,
+    },
+
+    600: {
+      grid: {
+        rows: 2,
+        fill: "row",
+      },
+      slidesPerView: 2,
+    },
+    800: {
+      grid: {
+        rows: 2,
+        fill: "row",
+      },
+      slidesPerView: 3,
+    },
+    1000: {
+      grid: {
+        rows: 2,
+        fill: "row",
+      },
+      slidesPerView: 4,
+    },
+  },
 });
